eae8a7b99067cf31fe6dc6cdf835796e3fb8dbf9
Jordan <jazborj@gmail.com>
2021-10-16 11:45:33 +0200

    last lessons

--- New Unity Project (1)/Assets/scripts/PlayerController.cs
+++ New Unity Project (1)/Assets/scripts/PlayerController.cs
@@ -7,21 +7,50 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float m_speed = 12f;
     Vector3 m_velocity;
     CharacterController m_characterController;
+    public Transform groundCheck;
+    public LayerMask groudMask;
+    RaycastHit hit;
+
     void PlayerMove()
     {
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
         Vector3 move = transform.right * x + transform.forward * z;
-        m_characterController.Move(move);
+        m_characterController.Move(move * m_speed * Time.deltaTime);
+    }
+    void updateSpeed()
+    {
+         
+        if (Physics.Raycast(groundCheck.position, transform.TransformDirection(Vector3.down),
+            out hit, 2f, groudMask))
+        {
+            string terrainType = "";
+            terrainType = hit.collider.gameObject.tag;
+            switch(terrainType)
+            {
+                case "SlowerPlane":
+                    m_speed = 12f / 2f;
+                    break;
+                case "FasterPlane":
+                        m_speed = 12f * 1.5f;
+                    break;
+                default:
+                    m_speed = 12f;
+                    break;
+            }
+        }
     }
     void Start()
     {
         m_characterController = GetComponent<CharacterController>();
+        m_speed = 12f;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         PlayerMove();
+        updateSpeed();
     }
 }
