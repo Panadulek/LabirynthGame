a21fe93b426e14f675bbad6f28aa4b642ff5ff4b
Jordan <jazborj@gmail.com>
2021-10-16 12:31:05 +0200

    sasadsa
    ewqewq

---
+++ New Unity Project (1)/Assets/scripts/GameManager.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager gameManager;
+    [SerializeField] uint time;
+    bool pause;
+    bool endGame;
+    bool win;
+    bool mode;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mode = true;
+        pause = false;
+        endGame = false;
+        win = false;
+        if (gameManager == null)
+        {
+            gameManager = this;
+        }
+       
+        /*fun(--i) a fun(i--)
+         * 
+         */
+     
+        time = 100;
+        Debug.Log("Time:" + time + " s");
+        InvokeRepeating("Stoper", 1, 1);
+    }
+    void Stoper()
+    {
+        time--;
+        Debug.Log("Time:" + time + " s");
+        if (time == 0)
+        {
+            endGame = true;
+        }
+        if(endGame)
+        {
+            EndGame();
+        }
+    }
+    public void EndGame()
+    {
+        CancelInvoke("Stoper");
+        if(win)
+        {
+            Debug.Log("win");
+        }
+        else
+        {
+            Debug.Log("lose");
+        }
+
+    }
+    public void chaneStaus ()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if (mode)
+            {
+                Debug.Log("Pause Game");
+                Time.timeScale = 0f;
+            }
+            else
+            {
+                Debug.Log("Resume Game");
+                Time.timeScale = 1f;
+      
+            }
+            mode = !mode;
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        chaneStaus();
+    }
+}
